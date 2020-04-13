cb523add4e9a050d81b42cc69de721482d812525
Menno <my.bil@student.avans.nl>
2020-04-13 12:55:20 +0200

    [BUGFIX] Player movement

--- PlayerBody.gd
+++ PlayerBody.gd
@@ -10,7 +10,7 @@ var targetPos = Vector2()
 func _ready():
 	$PlayerSprite.rotate(0.5 * PI)
 
-
+# Gets called every frame
 func _physics_process(delta):
 	targetPos = get_global_mouse_position()
 	lookAtMouse()
@@ -18,7 +18,7 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity * delta)
 
 
-#look at mouse
+# Looks at the mouse
 func lookAtMouse():
 	var direction = get_angle_to(targetPos)
 	if abs(direction) < turn_speed:
@@ -28,7 +28,7 @@ func lookAtMouse():
 		if direction < 0: rotation -= turn_speed
 
 
-#movement
+# Handles all the inputs for the player
 func getInputs():
 	velocity = Vector2()
 	
@@ -36,11 +36,10 @@ func getInputs():
 		velocity.x = -SPEED 
 	if Input.is_action_pressed("ui_up"):
 		velocity.x = SPEED 
-		if self.position.distance_squared_to(targetPos) < 1160000 and self.position.distance_squared_to(targetPos) > 1150000:
-			velocity = Vector2(0, 0).rotated(rotation)
+		if self.position.distance_squared_to(targetPos) < 1158000 and self.position.distance_squared_to(targetPos) > 1150000:
+			velocity.x = 0
 	if Input.is_action_pressed("ui_left"):
 		velocity.y = -SPEED 
 	if Input.is_action_pressed("ui_right"):
 		velocity.y = SPEED 
-
 	velocity = velocity.rotated(rotation)
