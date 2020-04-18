b4c362bc5cd9107cd7d9a9c5ec2996b4fe058f09
Menno <my.bil@student.avans.nl>
2020-04-18 12:21:32 +0200

    [MOVED] files moved

--- scripts/Stamina.gd
+++ scripts/Stamina.gd
@@ -1,15 +1,15 @@
 extends Control
 
-
 # Gets called once
 func _ready():
 	$TextureProgress.max_value = get_node("/root/Game/Gameplay/Entity's/Player/PlayerBody").MAX_STAMINA
+	$TextureProgress.step = 0.1
 
 # Gets called every frame
 func _process(delta):
 	set_global_position(get_node("/root/Game/Gameplay/Entity's/Player/PlayerBody").position)
 	setStamina()
 
-# Sets the stamina of the image to the stamina of the player
+# Sets the stamina of the progressbar to the stamina of the player
 func setStamina():
 	$TextureProgress.value = get_node("/root/Game/Gameplay/Entity's/Player/PlayerBody").stamina
